ed703ceca6f7a049f2a1dceb078a4b0c7560f419
Danni St.Claire-Coe <47538480+danniscoe@users.noreply.github.com>
2021-02-18 21:29:09 -0600

    JS Final

--- ToDoList.js
+++ ToDoList.js
@@ -1,8 +1,44 @@
-document.getElementById("add").onclick = function () {
+let todoItems = [];
 
-    var node = document.createElement("Li");
-    var text = document.getElementById("user_input").value;
-    var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById("list_item").appendChild(node);
+function renderTodo(todo) {
+    const list = document.querySelector('.js-todo-list');
+
+    const isChecked = todo.checked ? 'done' : '';
+    const node = document.createElement("li");
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  ;
+
+    list.append(node);
 }
+
+function addTodo(text) {
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now(),
+    };
+
+    todoItems.push(todo);
+    renderTodo(todo);
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = document.querySelector('.js-todo-input');
+
+    const text = input.value.trim();
+    if (text !== '') {
+        addTodo(text);
+        input.value = '';
+        input.focus();
+    }
+});
